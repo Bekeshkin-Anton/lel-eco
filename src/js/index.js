5c1d7a8545e98eb4082f357863cbfe76c24bd6f8
Daniil Mitrofanov <dmitrofanov@innoseti.ru>
2022-09-03 16:34:21 +0200

    7, 8 setions adaptive layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,1 @@
-import '../css/style.css';
 import '../css/style.scss';
-
-import header from './steps/header';
-import popup from './steps/popup';
-// import mask from './steps/mask';
-import validationForms from './steps/validationForms';
-
-header();
-// mask();
-popup();
-validationForms();
